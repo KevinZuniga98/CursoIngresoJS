e85d240f1f81305b3a67f288d0ab209db649bd9e
KevinZuniga98 <52494862+KevinZuniga98@users.noreply.github.com>
2019-07-04 12:31:25 -0300

    Add files via upload
    clase 2

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+  var importe;
+  var descuento;
+  var resueltado;
+
+  importe = parseFloat(document.getElementById("importe").value);
+
+  descuento = (importe / 100 ) * 25;
+
+  resultado = importe - descuento
+
+  document.getElementById("resultado").value = resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,9 +1,9 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
+/*Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-    var nombre = prompt("Ingrese su nombre:");;
-    
+    var nombre = prompt("Ingrese su nombre:");
+
     alert(nombre);
 }
 
